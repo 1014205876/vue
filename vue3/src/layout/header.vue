3cefa2b07f30bf5298547203042e50bf75e6d0bf
1014205876@qq.com <1014205876@qq.com>
2020-03-13 19:12:50 +0800

    ok

---
+++ vue3/src/layout/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="head">
+        头部
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'appHeader',
+        components: {
+        }
+    }
+</script>
+
+<style scoped>
+    .head{
+        background:red;
+    }
+</style>

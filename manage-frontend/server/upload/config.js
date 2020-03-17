c0ad37415d3b119fd9977b024d2a956d833302cb
wangzheng <wz@96325>
2020-03-17 14:16:56 +0800

    12

--- manage-frontend/server/upload/config.js
+++ manage-frontend/server/upload/config.js
@@ -1,4 +1,6 @@
 var config = {
+    SecretId: 'AKIDSluoSj3fGfx314qSt4Bi6F7k9RUTMlnp',
+    SecretKey: 'rVv99uTmzyiFWZNo0KVTDl36nEahdKrJ',
     Bucket: '1014-1259043953',
     Region: 'ap-beijing',
 };
